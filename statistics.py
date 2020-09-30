39279a613356bd2ecb4082c56609605b3efb8c55
Tolsager <victortolsagerolesen1@gmail.com>
2020-09-30 12:29:22 +0200

    confidence interval

--- statistics.py
+++ statistics.py
@@ -6,6 +6,11 @@ won = np.load('')
 lost = np.load('')
 end_position = np.load('')
 
-def confidence_interval(array):
+def mean_confidence_interval(array):
     mean = np.mean(array)
-    critical_value =
+    critical_value = 1.96
+    n = 100_000
+    standard_error = np.sqrt(np.var(array)/n)
+
+    confidence_interval = (mean - critical_value * standard_error, mean + critical_value * standard_error)
+    return confidence_interval
